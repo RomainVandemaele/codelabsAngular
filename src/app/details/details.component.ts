352ef552d75bb86416596967b07c1328c3aaf6ab
romain <romain.v@live.be>
2024-05-08 14:09:01 +0200

    adding houseLocation details with route getting house from service and id from route

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+import {HousingService} from '../housing.service';
+import {HousingLocation} from '../housinglocation';
+import {HousingLocationComponent} from "../housing-location/housing-location.component";
 
 @Component({
   selector: 'app-details',
@@ -9,5 +13,14 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute)
+  housingService: HousingService = inject(HousingService);
+  housingLocationId: number = -1;
+  housingLocation: HousingLocation | undefined;
+
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
 
 }
